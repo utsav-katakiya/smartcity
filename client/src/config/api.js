e322b0de3ccde11944faac8c0cb9aa53306f7dd3
utsav katakiya <utsavkatakiya98@gmail.com>
2026-04-11 02:10:02 +0530

    fix: API base URL for production

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,1 +1,5 @@
+if (!import.meta.env.VITE_API_URL) {
+  console.error("API URL not defined in environment variables!");
+}
+
 export const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
